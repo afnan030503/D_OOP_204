11cfdf3c3b9bc18071e5d4ccd64aee63e6e4bdb8
afnan030503 <muhammadafnanmaulidi@gmail.com>
2023-04-10 15:22:00 +0700

    spasi enter

--- OOP/OOP.cpp
+++ OOP/OOP.cpp
@@ -16,9 +16,35 @@ private:
     int sks;
     string kode;
     string namaMK;
+public:
+    void inputMk() {
+
+        cout << "\nMasukan Jumlah SKS= ";
+        cin >> sks;
+        cout << "\nMasukan Kode MK= ";
+        cin >> kode;
+        cout << "\nMasukan Nama MK= ";
+        cin >> namaMK;
+    }
+
+    void tampilMK() {
+        cout << "\nJumlah SKS= " << sks;
+        cout << "\nKode MK= " << kode;
+        cout << "\nNama MK= " << namaMK << endl;
+
+    }
 };
 
-int main()
-{
+int main() {
+    Mahasiswa mhs;
+    mataKuliah mk;
+    cout << "\nMasukkan NIM mahasiswa= ";
+    cin >> mhs.nim;
+    cout << "\nMAsukkan Nama Mahasiswa= ";
+    cin >> mhs.nama;
 
+    mk.inputMk();
+    mhs.tampil();
+    cout << endl;
+    mk.tampilMK();
 }
